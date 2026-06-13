217cd619ae00558b97a201a8f35114af1d83193b
Meet Patel <meetpatel3492@gmail.com>
2026-06-13 16:08:42 -0500

    Initial setup done

--- nix/home.nix
+++ nix/home.nix
@@ -13,6 +13,33 @@ in
 
   programs.home-manager.enable = true;
 
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    history.size = 10000;
+    initContent = ''
+      alias hms="home-manager switch --flake ~/develop/dotfiles/nix#mpatel -b backup --show-trace"
+      alias start-qtile="dbus-run-session qtile start -s wayland"
+      alias pbcopy="wl-copy"
+      alias pbpaste="wl-paste"
+      alias v="nvim"
+      command -v lsd > /dev/null && alias ls="lsd --group-dirs first"
+      command -v lsd > /dev/null && alias tree="lsd --tree"
+      alias l="ls -la"
+      export BAT_THEME_DARK="ansi"
+      export BAT_THEME_LIGHT="ansi"
+      alias bat="bat --plain"
+      command -v cat > /dev/null && alias cat="bat --pager=never"
+      command -v less > /dev/null && alias less="bat"
+      source <(fzf --zsh)
+      eval "$(zoxide init zsh)"
+      alias j="z"
+      eval "$(oh-my-posh init zsh --config $HOME/.config/oh-my-posh/prompt.json)"
+    '';
+  };
+
   programs.chromium = {
     enable = true;
     package = pkgs.brave;
@@ -57,11 +84,26 @@ in
     nerd-fonts.hurmit
     # display stuff to be move to module 
     (config.lib.nixGL.wrap ghostty)
-    python314Packages.qtile
-    python314Packages.qtile-extras
+    (config.lib.nixGL.wrap python314Packages.qtile)
+    (config.lib.nixGL.wrap python314Packages.qtile-extras)
     xdg-desktop-portal
     xdg-desktop-portal-wlr
     xdg-desktop-portal-gtk
+
+    # audio
+    pavucontrol
+    
+    tmux
+    cargo
+    zoxide
+    bat
+    lsd
+    delta
+    wget
+    wl-clipboard
+    oh-my-posh
+    fzf
+    yazi
   ];
 
   home.sessionVariables = {
@@ -93,6 +135,21 @@ in
 
     ".config/git/config" = {
       source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/gitconfig/.gitconfig";
+    };
+
+    ".tmux/plugins/tpm" = {
+      source = builtins.fetchGit {
+        url = "https://github.com/tmux-plugins/tpm.git";
+        shallow = true;
+        rev = "e261deb1b47614eed3400089ce7197dc68acc4eb";
+      };
+    };
+
+    ".config/oh-my-posh/prompt.json" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/oh-my-posh/prompt.json";
+    };
+    ".config/tmux" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${dotfilesDir}/tmux";
     };
   };
 
--- qtile/config.py
+++ qtile/config.py
@@ -1,32 +1,62 @@
 # Minimal qtile config
-    from libqtile import bar, layout, widget, hook
-    from libqtile.config import Key, Group, Screen
-    from libqtile.lazy import lazy
-    mod = "mod4"
+from libqtile import bar, layout, widget
+from libqtile.config import Key, Group, Screen
+from libqtile.lazy import lazy
+from libqtile.backend.wayland import InputConfig
 
-    keys = [
-      Key([mod], "h", lazy.layout.left()),
-      Key([mod], "l", lazy.layout.right()),
-      Key([mod], "Return", lazy.spawn("ghostty")),
-      Key([mod], "b", lazy.spawn("/home/mpatel/.nix-profile/bin/brave")),
-      Key([mod, "control"], "r", lazy.reload_config()),
-      Key([mod, "control"], "q", lazy.shutdown()),
-    ]
+mod = "mod4"
 
-    groups = [Group(i) for i in "123456789"]
+wl_input_rules = {
+    "type:touchpad": InputConfig(
+        natural_scroll=True,
+        tap=True,
+        accel_profile="flat",
+        pointer_accel=0.5,
+        scroll_factor=0.1
+    )
+}
 
-    layouts = [layout.Max(), layout.Stack(num_stacks=2)]
+groups = [Group(i) for i in "123456789"]
 
-    screens = [
-      Screen(top=bar.Bar([widget.GroupBox(), widget.WindowName(), widget.Clock(format='%Y-%m-%d %a %H:%M')], 24))
-    ]
+keys = [
+    Key([mod], "h", lazy.layout.left()),
+    Key([mod], "l", lazy.layout.right()),
+    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod], "Return", lazy.spawn("ghostty")),
+    Key([mod], "b", lazy.spawn("brave")),
+    Key([mod, "control"], "r", lazy.reload_config()),
+    Key([mod, "control"], "q", lazy.shutdown()),
 
-    main = None
-    follow_mouse_focus = True
-    bring_front_click = False
-    cursor_warp = False
+]
 
-    @hook.subscribe.startup_once
-    def autostart():
-      import subprocess
-      subprocess.call(['sh', '-c', 'xrandr --auto &'])
+for i in groups:
+    keys.append(
+        Key(
+            [mod],
+            i.name,
+            lazy.group[i.name].toscreen(),
+            desc=f"Switch to group {i.name}"
+        )
+    )
+    keys.append(
+        Key(
+            [mod, "shift"],
+            i.name,
+            lazy.window.togroup(i.name, switch_group=True),
+            desc=f"Move window to group {i.name}"
+        )
+    )
+
+
+
+layouts = [layout.Max(), layout.Stack(num_stacks=2)]
+
+screens = [
+    Screen(top=bar.Bar([widget.GroupBox(), widget.WindowName(
+    ), widget.Clock(format='%Y-%m-%d %a %H:%M')], 24))
+]
+
+main = None
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
